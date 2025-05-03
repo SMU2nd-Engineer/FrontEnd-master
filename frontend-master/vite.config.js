fb0f81284a5d1795ae0045654543d645761f02d2
yellow-pang <troh0322@naver.com>
2025-05-03 10:20:58 +0900

    README edit, alias config add, routes folder add
    alias 설정 추가, 라우팅 공통 폴더 추가, README 파일 수정

--- frontend-master/vite.config.js
+++ frontend-master/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "src/common"),
+      "@domain": path.resolve(__dirname, "src/domain"),
+      "@product": path.resolve(__dirname, "src/domain/product"),
+      "@user": path.resolve(__dirname, "src/domain/user"),
+    },
+  },
+});
