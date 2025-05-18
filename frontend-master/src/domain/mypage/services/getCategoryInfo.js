6c1c22352681698eb98c4f1f2365b008d28ab9a8
yellow-pang <troh0322@naver.com>
2025-05-18 21:37:45 +0900

    Remove unnecessary files after merging get requests
    Remove unnecessary files after merging get requests

--- frontend-master/src/domain/mypage/services/getCategoryInfo.js
+++
@@ -1,8 +0,0 @@
-import axiosInstance from "@/lib/axiosInstance";
-
-export const getCategoryInfo = async () => {
-  const res = await axiosInstance.get("/user/getCategory", {
-    withCredentials: true,
-  });
-  return res.data;
-};
