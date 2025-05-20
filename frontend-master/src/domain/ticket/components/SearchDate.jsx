77c80165c7b7ed051108f6c29c8eef31fb9e6b87
이가현 <gahyun970121@gmail.com>
2025-05-20 13:03:56 +0900

    ticketPage BackUp

---
+++ frontend-master/src/domain/ticket/components/SearchDate.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import SelectDateCalendar from "../components/SelectDateCalendar";
+
+const SearchDate = ({ onDateChange }) => {
+  const [startDate, setStartDate] = useState(null); // 시작날짜 지정
+  const [endDate, setEndDate] = useState(null); // 종료날짜 지정
+
+  const handleStartChange = (date) => {
+    setStartDate(date); // 선택한 날짜를 시작날짜로 셋팅
+
+    // 시작일이 종료일보다 늦으면 종료일 초기화
+    if (endDate && date > endDate) {
+      setEndDate(null);
+    }
+    // 부모 컴포넌트에게 날짜 알려쥐기
+    onDateChange(date, endDate);
+  };
+
+  const handleEndChange = (date) => {
+    setEndDate(date); // 선택한 날짜를 종료날짜로 셋팅
+    onDateChange(startDate, date); // 부모 컴포넌트에게 날짜 알려주기
+  };
+
+  return (
+    <div>
+      <SelectDateCalendar
+        startDate={startDate}
+        endDate={endDate}
+        onStartChange={handleStartChange}
+        onEndChange={handleEndChange}
+      />
+    </div>
+  );
+};
+
+export default SearchDate;
