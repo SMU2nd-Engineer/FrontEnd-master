8d9cfcd3652759e7e12bac3cc0ac6166e7412625
이가현 <gahyun970121@gmail.com>
2025-05-27 15:11:44 +0900

    mypage favoriteCategory css

--- frontend-master/src/domain/mypage/components/FavoriteCategory.jsx
+++ frontend-master/src/domain/mypage/components/FavoriteCategory.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import "../style/favorite.css";
 
 export default function FavoriteCategory({ category, register }) {
   return (
-    <>
+    <div className="favorite-main">
       <input
         type="checkbox"
         {...register("favorites[]")}
         value={`FAV_${category.subIdx}`}
         id={`${category.subIdx}`}
+        className="favorite-CheckBox"
       />
-      <label htmlFor={`${category.subIdx}`}> {category.name} </label>
-    </>
+      <label htmlFor={`${category.subIdx}`} className="favorite-label">
+        {" "}
+        {category.name}{" "}
+      </label>
+    </div>
   );
 }
--- frontend-master/src/domain/mypage/page/MyEditFavoritePage.jsx
+++ frontend-master/src/domain/mypage/page/MyEditFavoritePage.jsx
@@ -6,7 +6,6 @@ export default function MyEditFavoritePage() {
   return (
     <div>
       <MyPageLink />
-      <h2>관심있는 좋아하는 아티스트를 선택해주세요.</h2>
       <FavoritesSelector mode="edit" />
     </div>
   );
