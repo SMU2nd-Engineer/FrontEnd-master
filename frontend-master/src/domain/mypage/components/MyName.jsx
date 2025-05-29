8f71b3a8e49a7c26a4d57788b4bc4e80aad711a3
yellow-pang <troh0322@naver.com>
2025-05-29 15:11:38 +0900

    Modify useEffect to be called only once
    Modify the data code while modifying the logic to call useEffect only once.

--- frontend-master/src/domain/mypage/components/MyName.jsx
+++ frontend-master/src/domain/mypage/components/MyName.jsx
@@ -1,15 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { getMyPageData } from "../services/getMyPageDate";
+import React from "react";
 import { MyMainName } from "../style/MyMainPageDesign";
 
-export default function MyName() {
-  const [userName, setUserName] = useState("");
-  useEffect(() => {
-    const getUserName = async () => {
-      const userInfo = await getMyPageData("USER_INFO");
-      setUserName(userInfo.name);
-    };
-    getUserName();
-  }, []);
-  return <MyMainName>{userName}님</MyMainName>;
+export default function MyName({ name }) {
+  return <MyMainName>{name}님</MyMainName>;
 }
