58798eaad6fecf25bf2c80c50558adb9222c7e51
zenbms95 <bms051234@gmail.com>
2025-05-31 15:39:34 +0900

    Board file uplaod
    25/5/30 2차
    
    -- 수정
    
    공통사항
    : 게시판 리스트페이지 디자인 수정중

--- frontend-master/src/domain/board/components/BoardPageFooter.jsx
+++ frontend-master/src/domain/board/components/BoardPageFooter.jsx
@@ -3,6 +3,7 @@ import { getCategoryIdx } from "@/utils/CategoryHandler";
 import { Select } from "@chakra-ui/react";
 import { useState } from "react";
 import SearchSelectBox from "@/components/SearchSelectBox";
+import * as board from "../styles/BoardListPageDesign";
 
 const BoardPageFooter = ({handleOnclick}) => {
   const [searchTypes, setSearchTypes] = useState(""); // 구분자(제목+내용/작성자)  
@@ -15,28 +16,38 @@ const BoardPageFooter = ({handleOnclick}) => {
 
   // 화면에 출력되는 내용
   return (
-    <div className="board_page_footer">
-      {/* 구분자: 제목+내용 / 작성자 */} 
-      <SearchSelectBox 
-        handleChange={(e) => setSearchTypes(e.target.value)}/>
+    <board.Board_listPageFooter>
+      <board.Board_SearchSelectBox>
+        {/* 구분자: 제목+내용 / 작성자 */} 
+        <SearchSelectBox 
+          handleChange={(e) => setSearchTypes(e.target.value)}/>
+      </board.Board_SearchSelectBox>
 
-      {/* 카테고리 선택 : 잡담 / 팝니다 / 삽니다 / 기타 */}  
-      <SelectBox 
-        id="categpry"
-        name="categpry" 
-        category_idx={getCategoryIdx("BOARD")}
-        handleChange={(e) => setCategpries(e.target.value)}/>
+      <board.Board_CategorySelectBox>
+        {/* 카테고리 선택 : 잡담 / 팝니다 / 삽니다 / 기타 */}  
+        <SelectBox 
+          id="categpry"
+          name="categpry" 
+          category_idx={getCategoryIdx("BOARD")}
+          handleChange={(e) => setCategpries(e.target.value)}/>
+      </board.Board_CategorySelectBox>
 
-      {/* 검색어 입력창 */}
-      <input 
-        type="text"
-        placeholder="검색어 입력창"
-        value={searchKeywords}
-        onChange={(e) => setSearchKeywords(e.target.value)}       
-      />
-      {/* 카테고리 선택하고 키워드로 검색어를 입력한 후 누르면 실행되는 버튼 */}
-      <button onClick={onClick}>게시글 검색</button>
-    </div>
+      <board.Board_SearchInput>
+        {/* 검색어 입력창 */}
+        <input 
+          type="text"
+          placeholder="검색어 입력창"
+          value={searchKeywords}
+          onChange={(e) => setSearchKeywords(e.target.value)}       
+        />
+      </board.Board_SearchInput>
+
+      <board.Board_SearchButton>
+        {/* 카테고리 선택하고 키워드로 검색어를 입력한 후 누르면 실행되는 버튼 */}
+        <button onClick={onClick}>검색</button>
+      </board.Board_SearchButton>
+
+    </board.Board_listPageFooter>
 
   );
 };
--- frontend-master/src/domain/board/styles/BoardListPageDesign.jsx
+++ frontend-master/src/domain/board/styles/BoardListPageDesign.jsx
@@ -4,14 +4,15 @@ export const Board_top = styled.div`
   display: flex;
   justify-content: space-between;
   width: 100%;
-  height: 30%;
+  height: 100%;
+  padding: 20px;
   button{
-    /* border : 1px solid black; */
     background-color: #f0b8b8;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 -2px 4px rgba(0, 0, 0, 0.05);
     border-radius: 5px;
+    width: 10%;
+    height: 50%;
   }
-
 `;
 
 export const Board_main = styled.div`
@@ -19,7 +20,6 @@ export const Board_main = styled.div`
   height: 800px;
   text-align: center;
   justify-content: end;
-
 `;
 
 export const Board_middleList = styled.div`
@@ -65,3 +65,47 @@ export const Board_contentsListMain = styled.div`
   flex-direction: column;
 `;
 
+export const Board_listPageFooter = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 12px 15px;
+  border-radius: 30px;
+  gap: 30px;
+`;
+
+export const Board_SearchSelectBox = styled.div`
+  select {
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 -2px 4px rgba(0, 0, 0, 0.05);
+    border-radius: 5px;
+    width: 120%;
+    height: 50%;
+  }
+`;
+
+export const Board_CategorySelectBox = styled.div`
+  select {
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 -2px 4px rgba(0, 0, 0, 0.05);
+  border-radius: 5px;
+  width: 120%;
+  height: 50%;
+  }
+`;
+
+export const Board_SearchInput = styled.div`
+  input {
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 -2px 4px rgba(0, 0, 0, 0.05);
+  border-radius: 5px;
+  width: 105%;
+  height: 50%;  
+  }
+`; 
+
+export const Board_SearchButton = styled.div`
+  button{
+  background-color: #f0b8b8;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 -2px 4px rgba(0, 0, 0, 0.05);
+  border-radius: 5px;
+  width: 110%;
+  height: 110%;
+  }
+`;
