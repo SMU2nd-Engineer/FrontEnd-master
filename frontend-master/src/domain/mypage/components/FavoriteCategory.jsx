ed8d44a888c3304f60eb0d3449dcfe3c85c869bd
yellow-pang <troh0322@naver.com>
2025-06-03 00:27:52 +0900

    before favroit edit commit
    관심사 수정 전 커밋

--- frontend-master/src/domain/mypage/components/FavoriteCategory.jsx
+++ frontend-master/src/domain/mypage/components/FavoriteCategory.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import * as Favorite from "../style/FavoriteDesign";
 
 export default function FavoriteCategory({ category, register }) {
+  const getParentName = (subIdx) => {
+    if (subIdx >= 1000 && subIdx < 2000) return "상품";
+    if (subIdx >= 2000 && subIdx < 3000) return "장르";
+    if (subIdx >= 3000 && subIdx < 4000) return "티켓";
+    return "기타";
+  };
+
+  const parentName = getParentName(category.subIdx);
+  const displayName = `${category.name} (${parentName})`;
+
   return (
     <Favorite.FavoriteMain>
       <Favorite.FavoriteLabel htmlFor={`${category.subIdx}`}>
