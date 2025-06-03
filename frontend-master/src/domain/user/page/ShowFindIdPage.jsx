01596356cb2e307fba5290a0dd722a8e5e76f9ea
yellow-pang <troh0322@naver.com>
2025-06-03 18:04:31 +0900

    page design add, Change to modal
    회원 가입 팝업 컴포넌트 대신 전역 모달로 변경
    
    id 찾은 후 페이지 디자인 누락으로 추가

--- frontend-master/src/domain/user/page/ShowFindIdPage.jsx
+++ frontend-master/src/domain/user/page/ShowFindIdPage.jsx
@@ -1,24 +1,33 @@
-import Button from "@/components/Button";
 import React from "react";
 import { useLocation } from "react-router-dom";
+import {
+  BackButton,
+  EmphasizedId,
+  Header,
+  MainContainer,
+  Message,
+} from "../style/ShowFindIdPageDesign";
 
 export default function ShowFindIdPage() {
   const location = useLocation();
   const findId = location.state?.findId;
   return (
-    <div>
-      <h2>아이디 찾기</h2>
+    <MainContainer>
+      <Header>아이디 찾기</Header>
       {findId.socialLogin !== "regularLogin" ? (
-        <p>소셜 로그인 계정입니다. 소셜 로그인 해주세요.</p>
+        <Message>소셜 로그인 계정입니다. 소셜 로그인 해주세요.</Message>
       ) : (
-        <p>당신의 아이디는 : {findId?.id}</p>
+        <Message>
+          당신의 아이디는 : <EmphasizedId>{findId?.id}</EmphasizedId>
+        </Message>
       )}
-      <Button
-        text={"로그인 페이지로 돌아가기."}
+      <BackButton
         onClick={() => {
           window.location.href = "/user/login";
         }}
-      />
-    </div>
+      >
+        로그인 페이지로 돌아가기
+      </BackButton>
+    </MainContainer>
   );
 }
