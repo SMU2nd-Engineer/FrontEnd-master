afb699e83252e6920155a961f5f354de03a2f301
kwsubin <kwsubin@gmail.com>
2025-06-04 18:27:40 +0900

    typo update

--- frontend-master/src/domain/products/styles/ImageSliderDesign.jsx
+++ frontend-master/src/domain/products/styles/ImageSliderDesign.jsx
@@ -27,6 +27,18 @@ export const Arrow = styled.button`
   z-index: 1;
 `;
 
+// export const Slide = styled.div`
+//   position: absolute;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   row-gap: 20px;
+  
+//   transition: opacity 0.3s ease-in-out;
+//   &.active {
+//     opacity: 1;
+//   }
+// `;
 
 export const SlideImage = styled.img`
   width: 100%;
@@ -36,7 +48,7 @@ export const SlideImage = styled.img`
 `;
 
 export const ThumbnailWrapper = styled.div`
-  margin-top: 5px;
+  margin-top: 12px;
   display: flex;
   justify-content: center;
   gap: 8px;
@@ -49,7 +61,6 @@ export const ThumbnailImage = styled.img`
   object-fit: cover;
   cursor: pointer;
   border-radius: 4px;
-  
 
   border: ${({ $isActive }) => ($isActive ? "2px solid #333" : "1px solid transparent")};
   opacity: ${({ $isActive }) => ($isActive ? 1 : 0.6)};
--- frontend-master/src/domain/products/styles/ProductDetailDesign.jsx
+++ frontend-master/src/domain/products/styles/ProductDetailDesign.jsx
@@ -33,7 +33,15 @@ export const ThumbnailBox = styled.div`
   align-items: center;
   margin-bottom: 20px;
 `
+export const OtherImages = styled.div`
+  display: flex;
+  gap: 12px; /* 이미지 사이 간격 */
+  overflow-y: auto;
 
+  img{
+    width: 60px;
+  }
+`
 
 
 // 상품명, 가격, 판매자명
